90161ca59c9c468b387aba694722fe00c3b9c83e
jdhofmann <justindhofmann@gmail.com>
2020-09-16 17:06:51 -0400

    done

---
+++ app/controllers/school_class_controller.rb
@@ -0,0 +1,21 @@
+class SchoolClassController < ApplicationController
+  def index
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
